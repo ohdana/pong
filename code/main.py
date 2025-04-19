ad75091f8da52412e6a825d67dec6f718e4f9631
Dana Goosen <klymentovska@gmail.com>
2025-04-19 15:01:09 +0200

    Setup the main window

--- code/main.py
+++ code/main.py
@@ -1,1 +1,24 @@
 from settings import * 
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Pong')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            pygame.display.update()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
